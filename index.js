d7bf3f6aed983d02b9a5bee2067f0477c37fa888
alessandro <desmecad@hotmail.com>
2020-04-08 21:40:57 -0300

    Commit 4 - Criada a conexão com banco de dados

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const connection = require('./database/database')
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log('Conexão realizada com sucesso!')
+    })
+    .catch((msgErro) => {
+        console.log(msgErro)
+    })
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.get("/", (req, res) =>{
     res.render('index')
@@ -10,6 +24,12 @@ app.get("/", (req, res) =>{
 
 app.get("/perguntar", (req, res) => {
     res.render('perguntar')
+})
+
+app.post("/salvarpergunta", (req, res) => {
+    let titulo = req.body.titulo
+    let descricao = req.body.descricao
+    res.send('Titulo: ' + titulo + 'Descrição: ' + descricao)
 })
 
 app.listen(8081, () => {
