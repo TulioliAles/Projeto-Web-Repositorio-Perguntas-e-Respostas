653ae7a0cf309d06472b6712fcc898920337c4ef
alessandro <desmecad@hotmail.com>
2020-04-09 16:55:38 -0300

    Commit 6 - Implementação da criação da tabela no BD com Sequelize

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const connection = require('./database/database')
+const Pergunta = require('./database/Pergunta')
 
 connection
     .authenticate()
@@ -19,7 +20,11 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.get("/", (req, res) =>{
-    res.render('index')
+    Pergunta.findAll({raw: true}).then(perguntar => {
+        res.render('index', {
+            perguntas: perguntas
+        })        
+    })
 })
 
 app.get("/perguntar", (req, res) => {
@@ -29,7 +34,12 @@ app.get("/perguntar", (req, res) => {
 app.post("/salvarpergunta", (req, res) => {
     let titulo = req.body.titulo
     let descricao = req.body.descricao
-    res.send('Titulo: ' + titulo + 'Descrição: ' + descricao)
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() => {
+        res.redirect('/')
+    })
 })
 
 app.listen(8081, () => {
