2b64127f8479036a20992605b28ccbe032731171
TulioliAles <desmecad@hotmail.com>
2020-04-13 16:59:28 -0300

    Commit 12 - Mostra respostas gravadas na tela da pergunta

--- index.js
+++ index.js
@@ -51,12 +51,32 @@ app.get('/pergunta/:id', (req, res) => {
         where: {id: id}
     }).then(pergunta => {
         if(pergunta != undefined){
-            res.render('pergunta', {
-                pergunta: pergunta
+
+            Resposta.findAll({
+                where: {perguntaId: pergunta.id},
+                order: [
+                    ['id', 'DESC']
+                ]
+            }).then(respostas => {
+                res.render('pergunta', {
+                    pergunta: pergunta,
+                    respostas: respostas
+                })
             })
         }else{
             res.redirect('/')
         }
+    })
+})
+
+app.post('/responder', (req, res) => {
+    let corpo = req.body.corpo
+    let perguntaId = req.body.pergunta
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() => {
+        res.redirect('/pergunta/' + perguntaId)
     })
 })
 
