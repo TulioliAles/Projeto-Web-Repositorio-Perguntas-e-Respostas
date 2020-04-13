6dac0d41cf557073d71c3a7fb7af66b591197976
TulioliAles <desmecad@hotmail.com>
2020-04-13 15:34:07 -0300

    Commit 10 - Criado modulo que ira criar tabela 'respostas' e salvar as respostas na mesma

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const connection = require('./database/database')
 const Pergunta = require('./database/Pergunta')
+const Resposta = require('./database/Resposta')
 
 connection
     .authenticate()
